b9ad81f2a175879ba738d277661217fe012b7ee6
Andreea213 <andreeariosanu06@gmail.com>
2026-04-20 22:50:03 +0300

    Update Ex2_BST?.c

--- Ex2_BST?.c
+++ Ex2_BST?.c
@@ -9,6 +9,13 @@ struct node {
     struct node* rightChild;
 };
 
+void preOrderTraversal(struct node* current) {
+    if (current != NULL) {
+        printf("%d ", current->data);
+        preOrderTraversal(current->leftChild);
+        preOrderTraversal(current->rightChild);
+    }
+}
 
 struct node* insert(struct node* root, int data) {
     
@@ -45,7 +52,6 @@ struct node* insert(struct node* root, int data) {
 }
 
 bool esteBST(struct node* nod, long min, long max) {
-    // 1. Condiția de oprire: un arbore gol este BST
     if (nod == NULL) {
         return true;
     }
@@ -54,30 +60,20 @@ bool esteBST(struct node* nod, long min, long max) {
         return false;
     }
 
- 
     return esteBST(nod->leftChild, min, nod->data) && 
            esteBST(nod->rightChild, nod->data, max);
 }
 
- 
-
-
 int main() {
     struct node* root = NULL;
 
-   
    int v[7] = {20, 10, 30, 5, 15, 25, 35};
     root = insert(root, 20);
     for (int i=1;i<7;i++)
         insert(root, v[i]);
 
-  
-
-if (esteBST(root, LONG_MIN, LONG_MAX)) {
-        printf("Arborele este un BST valid!\n");
+    if (esteBST(root, LONG_MIN, LONG_MAX)) {
+        printf("Arborele este un BST valid.\n");
     } else {
         printf("Arborele NU este un BST valid.\n");
     }
-
-    return 0;
-}
